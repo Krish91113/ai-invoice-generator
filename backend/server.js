1cfc3861cb33dc89e05c2213aa9b794646c73710
Krish Baranwal <menikabaranwal121@gmail.com>
2025-12-25 20:38:01 +0530

    working

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import { clerkMiddleware } from '@clerk/express'
+
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 //MIDDLEWAREs
 app.use(cors());
-
+app.use(express.json({limit: '20mb'}));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
+app.use(clerkMiddleware())
 //DB
 
 
