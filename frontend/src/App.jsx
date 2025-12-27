bcbbbd7422386047c712f8752495ec25b68db6af
Krish Baranwal <menikabaranwal121@gmail.com>
2025-12-27 13:03:32 +0530

    Working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,32 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/clerk-react";
+import AppShell from "./component/AppShell";
+import Dashboard from "./pages/Dashboard";
 
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import { RedirectToSignIn, SignedIn, SignedOut } from '@clerk/clerk-react'
-
-const ClerkProtected = ({children}) =>{
+const ClerkProtected = ({ children }) => {
   <>
     <SignedIn>{children}</SignedIn>
     <SignedOut>
-        <RedirectToSignIn/>
+      <RedirectToSignIn />
     </SignedOut>
-  </>
-}
-
-
+  </>;
+};
 
 function App() {
   return (
-   <Routes>
-        <Route path='/' element={<Home />} />
+    <div className="min-h-screen max-w-full overflow-w-hidden">
+      <Routes>
+      <Route path="/" element={<Home />} />
 
-        {/* to protect the route */}
-   </Routes>
-  )
+      {/* to protect the route */}
+      <Route path="/app"element={<ClerkProtected><AppShell /></ClerkProtected>}/>
+      <Route index element={<Dashboard/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+    </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
