f0fbf215db33368e00ed1c7241a66bdadaf50be3
Krish Baranwal <menikabaranwal121@gmail.com>
2025-12-27 13:12:06 +0530

    Working

--- frontend/src/component/AppShell.jsx
+++ frontend/src/component/AppShell.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { appShellStyles } from '../assets/dummyStyles'
+import { useNavigate } from 'react-router-dom'
+import { useClerk, useUser } from '@clerk/clerk-react';
 
 
 function AppShell() {
+    const navigate =useNavigate();
+    const {signOut} = useClerk();
+    const {user} = useUser();
+
+    const [mobileOpen,setMobileOpen] = useState(false);
   return (
     <div className={appShellStyles.root}>
         <div className={appShellStyles.layout}>
             {/* sidebar */}
-            <aside className=''>
-
+            <aside className={`${appShellStyles.sidebar}`}>
+                
             </aside>
         </div>
     </div>
