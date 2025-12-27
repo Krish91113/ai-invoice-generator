72b893f7b7619be584333b7be37fc0a388161bfe
Krish Baranwal <menikabaranwal121@gmail.com>
2025-12-27 12:46:22 +0530

    Working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import { RedirectToSignIn, SignedIn, SignedOut } from '@clerk/clerk-react'
 
 const ClerkProtected = ({children}) =>{
   <>
-    
+    <SignedIn>{children}</SignedIn>
+    <SignedOut>
+        <RedirectToSignIn/>
+    </SignedOut>
   </>
 }
 
@@ -15,6 +19,8 @@ function App() {
   return (
    <Routes>
         <Route path='/' element={<Home />} />
+
+        {/* to protect the route */}
    </Routes>
   )
 }
