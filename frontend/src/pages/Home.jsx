c482f8ebc1c16e6175a5d6767e2e7722100d9027
Krish Baranwal <menikabaranwal121@gmail.com>
2025-12-28 09:30:26 +0530

    learning and doing

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import NavBar from '../component/NavBar'
-import Hero from '../component/Hero'
+import React from "react";
+import NavBar from "../component/NavBar";
+import Hero from "../component/Hero";
+import Features from "../component/Features";
 
 function Home() {
   return (
     <div>
-      
-      <NavBar/>
-      
-       <Hero/>
-    
+      <NavBar />
+      <main className="">
+        <Hero />
+        <div className="">
+          <Features />
+        </div>
+      </main>
     </div>
-    
-  )
+  );
 }
 
-export default Home
+export default Home;
